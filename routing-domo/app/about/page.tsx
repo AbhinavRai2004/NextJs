8edc8ce6f3cf41687c7b287f46204d64666d7933
Abhinav <raiabhinav.in@gmail.com>
2024-11-05 23:04:53 +0530

    routing-metadata

--- routing-domo/app/about/page.tsx
+++ routing-domo/app/about/page.tsx
@@ -1,3 +1,8 @@
+//static metadata configuration
+export const metadata = {
+  title: "About My Application"
+};
+
 export default function About() {
-    return <h1>Welcome to About Us Page</h1>
+  return <h1>Welcome to About Us Page</h1>;
 }
--- routing-domo/app/products/[productId]/page.tsx
+++ routing-domo/app/products/[productId]/page.tsx
@@ -1,7 +1,18 @@
-export default function Products({
-  params
-}: {
-  params: { productId: string };
-}) {
+import { Metadata } from "next";
+//dynamic metadata configuration
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product Details - ${params.productId}`,
+    description: `View product details for ${params.productId}`
+  };
+};
+
+export default function Products({ params }: Props) {
   return <h1>Product {params.productId} details</h1>;
 }
