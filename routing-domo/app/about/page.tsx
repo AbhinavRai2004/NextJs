a12f8382cafe14fdcabe67e2c3300fd1f1018ba4
Abhinav <raiabhinav.in@gmail.com>
2024-11-05 23:23:30 +0530

    link-component-navigation

--- routing-domo/app/about/page.tsx
+++ routing-domo/app/about/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 //static metadata configuration
 export const metadata = {
   title: "About My Application"
 };
 
 export default function About() {
-  return <h1>Welcome to About Us Page</h1>;
+  return (
+    <>
+      <h1>Welcome to About Us Page</h1>
+      <Link href="/">Go Back To Home</Link>
+    </>
+  );
 }
